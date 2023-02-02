634f98e976980a5cc80cfb1b48660c54b798b389
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2023-02-02 19:22:09 +0100

    added login function

--- src/pages/Login/headers.tsx
+++ src/pages/Login/headers.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { pages } from "@neoco/neoco-backoffice";
-// import { loginRequest } from "utils/requests";
 
 const { Login } = pages;
 
 const loginRequest = () => {
-  console.log("loginRequest");
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        user: {
+          name: "test",
+          email: "test@mail.com",
+        },
+        token: "test",
+      });
+      reject("error");
+    }, 1000);
+  });
 };
 
 const headers = {
@@ -16,7 +26,7 @@ const headers = {
       path: "/",
       unAuth: true,
       exact: true,
-      component: (props) => <Login {...props} onSubmit={loginRequest} />,
+      component: (props: any) => <Login {...props} onSubmit={loginRequest} />,
     },
   },
 };
