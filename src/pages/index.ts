c5c677edcbd27742b4fe46762e52daeb6f632760
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2023-02-03 14:24:16 +0100

    added first structure

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,11 @@
 import { headers as loginHeaders } from "./Login";
 import { headers as postsHeaders } from "./Posts";
 import { headers as writersHeaders } from "./Writers";
+import { headers as statisticsHeaders } from "./Statistics";
 
-export const headers = [loginHeaders, postsHeaders, writersHeaders];
+export const headers = [
+  loginHeaders,
+  postsHeaders,
+  writersHeaders,
+  statisticsHeaders,
+];
