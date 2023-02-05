a1ffa0dee034f7b5042c1cf1ed354425ba0861d2
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2023-02-05 18:42:05 +0100

    added first bar chart structure

--- src/pages/Statistics/headers.tsx
+++ src/pages/Statistics/headers.tsx
@@ -1,5 +1,25 @@
-import { Container, Grid, Paper, Typography } from "@mui/material";
+import { Grid, Paper, Typography } from "@mui/material";
+import { Bar } from "react-chartjs-2";
+import { CategoryScale } from "chart.js";
+import Chart from "chart.js/auto";
+
 import { moviesData } from "./fixtures/moviesData";
+
+const months = moviesData.viewsPerMonth.map((d) => d.month);
+const views = moviesData.viewsPerMonth.map((d) => d.views);
+
+const chartData = {
+  labels: months,
+  datasets: [
+    {
+      label: "Views per month",
+      data: views,
+      backgroundColor: "rgba(75,192,192,0.4)",
+      borderColor: "rgba(75,192,192,1)",
+      borderWidth: 1,
+    },
+  ],
+};
 
 const headers = {
   type: "Page",
@@ -11,41 +31,59 @@ const headers = {
       auth: true,
       exact: true,
       component: () => {
+        Chart.register(CategoryScale);
+
         return (
-          <Grid container spacing={2}>
-            <Grid item sm={12} md={6}>
-              <Paper
-                sx={{
-                  padding: "16px",
-                  margin: "16px",
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "16px",
-                }}
-                variant="outlined"
-                elevation={3}
-              >
-                <Typography variant="h6">Movies:</Typography>
-                <Typography variant="body1">{moviesData.movies}</Typography>
-              </Paper>
+          <>
+            <Grid container spacing={2}>
+              <Grid item sm={12} md={6}>
+                <Paper
+                  sx={{
+                    padding: "16px",
+                    margin: "16px",
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "16px",
+                  }}
+                  variant="outlined"
+                  elevation={3}
+                >
+                  <Typography variant="h6">Movies:</Typography>
+                  <Typography variant="body1">{moviesData.movies}</Typography>
+                </Paper>
+              </Grid>
+              <Grid item sm={12} md={6}>
+                <Paper
+                  sx={{
+                    padding: "16px",
+                    margin: "16px",
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "16px",
+                  }}
+                  variant="outlined"
+                  elevation={3}
+                >
+                  <Typography variant="h6">Views in a year:</Typography>
+                  <Typography variant="body1">{moviesData.views}</Typography>
+                </Paper>
+              </Grid>
             </Grid>
-            <Grid item sm={12} md={6}>
-              <Paper
-                sx={{
-                  padding: "16px",
-                  margin: "16px",
-                  display: "flex",
-                  alignItems: "center",
-                  gap: "16px",
-                }}
-                variant="outlined"
-                elevation={3}
-              >
-                <Typography variant="h6">Views in a year:</Typography>
-                <Typography variant="body1">{moviesData.views}</Typography>
-              </Paper>
-            </Grid>
-          </Grid>
+            <Bar
+              data={chartData}
+              options={{
+                plugins: {
+                  title: {
+                    display: true,
+                    text: "Movies views per month",
+                  },
+                  legend: {
+                    display: false,
+                  },
+                },
+              }}
+            />
+          </>
         );
       },
     },
