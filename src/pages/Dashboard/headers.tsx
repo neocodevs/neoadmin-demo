5521a2d5f14d7e8d86b643b8734ee3143d53c93a
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2023-02-07 19:19:16 +0100

    added new styles to dashboard

--- src/pages/Dashboard/headers.tsx
+++ src/pages/Dashboard/headers.tsx
@@ -1,5 +1,5 @@
-import { Grid, Paper, styled, Typography } from "@mui/material";
-import { Bar } from "react-chartjs-2";
+import { Box, Grid, Paper, Stack, styled, Typography } from "@mui/material";
+import { Bar, Line } from "react-chartjs-2";
 import { CategoryScale } from "chart.js";
 import Chart from "chart.js/auto";
 import CountUp from "react-countup";
@@ -15,8 +15,8 @@ const chartData = {
     {
       label: "Views per month",
       data: views,
-      backgroundColor: "rgba(75,192,192,0.4)",
-      borderColor: "rgba(75,192,192,1)",
+      backgroundColor: "#2575fc",
+      borderColor: "#f6f3ff",
       borderWidth: 1,
     },
   ],
@@ -36,60 +36,42 @@ const headers = {
 
         return (
           <>
-            <Grid container spacing={2}>
-              <Grid item sm={12} md={6}>
-                <StyledPaper
-                  sx={{
-                    padding: "16px",
-                    margin: "16px",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    gap: "16px",
-                  }}
-                  variant="outlined"
-                  elevation={3}
-                >
-                  <Typography variant="h2" fontWeight="bold">
-                    <CountUp end={postsData.posts} duration={2} />
-                  </Typography>
-                  <Typography variant="h6">Total posts</Typography>
-                </StyledPaper>
+            <Stack direction="column" spacing={2} padding={4}>
+              <Grid container spacing={2}>
+                <Grid item sm={12} md={6}>
+                  <StyledPaper variant="outlined" elevation={3}>
+                    <Typography variant="h2" fontWeight="bold">
+                      <CountUp end={postsData.posts} duration={2} />
+                    </Typography>
+                    <Typography variant="h6">Total posts</Typography>
+                  </StyledPaper>
+                </Grid>
+                <Grid item sm={12} md={6}>
+                  <StyledPaper variant="outlined" elevation={3}>
+                    <Typography variant="h2" fontWeight="bold">
+                      <CountUp end={postsData.views} duration={3} />
+                    </Typography>
+                    <Typography variant="h6">Views in a year</Typography>
+                  </StyledPaper>
+                </Grid>
+                <Grid item sm={12} md={2}></Grid>
               </Grid>
-              <Grid item sm={12} md={6}>
-                <StyledPaper
-                  sx={{
-                    padding: "16px",
-                    margin: "16px",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    gap: "16px",
+              <ChartContainer>
+                <Typography variant="h6" fontWeight="bold" color="#002b74">
+                  Posts views per month
+                </Typography>
+                <Bar
+                  data={chartData}
+                  options={{
+                    plugins: {
+                      legend: {
+                        display: false,
+                      },
+                    },
                   }}
-                  variant="outlined"
-                  elevation={3}
-                >
-                  <Typography variant="h2" fontWeight="bold">
-                    <CountUp end={postsData.views} duration={2} />
-                  </Typography>
-                  <Typography variant="h6">Views in a year</Typography>
-                </StyledPaper>
-              </Grid>
-            </Grid>
-            <Bar
-              data={chartData}
-              options={{
-                plugins: {
-                  title: {
-                    display: true,
-                    text: "Posts views per month",
-                  },
-                  legend: {
-                    display: false,
-                  },
-                },
-              }}
-            />
+                />
+              </ChartContainer>
+            </Stack>
           </>
         );
       },
@@ -101,4 +83,22 @@ export default headers;
 
 const StyledPaper = styled(Paper)(({ theme }) => ({
   background: "linear-gradient(to right, #6a11cb 0%, #2575fc 100%)",
+  padding: "16px",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "16px",
+  borderRadius: "16px",
+}));
+
+const ChartContainer = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: theme.spacing(2),
+  background: "#cce5ff",
+  padding: theme.spacing(4),
+  borderRadius: "16px",
+  margin: "16px 0",
 }));
